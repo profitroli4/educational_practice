472f07f285f03c551b0d4faefd9f6fe44abd3e50
danilprofiru <danilprofiru@gmail.com>
2024-06-26 19:04:20 +0300

    Добавлена инициализация БД, также добавлен парсер для вакансий  с сохранением в файле JSON без взаимодействия с Telegram-ботом и БД.

---
+++ telegram-bot/init_db.py
@@ -0,0 +1,38 @@
+import psycopg2
+from psycopg2 import sql
+
+def create_tables():
+    conn = psycopg2.connect(
+        dbname='practice',
+        user='danil',
+        password='12345',
+        host='localhost',
+        port='5432'
+    )
+    cursor = conn.cursor()
+
+    create_vacancies_table_query = """
+    CREATE TABLE IF NOT EXISTS vacancies (
+        id SERIAL PRIMARY KEY,
+        hh_id VARCHAR(50) UNIQUE NOT NULL,
+        title TEXT NOT NULL,
+        link TEXT NOT NULL,
+        employer TEXT,
+        salary JSONB,
+        date_posted TIMESTAMP,
+        description TEXT,
+        requirements TEXT
+    );
+    """
+    
+
+    
+    cursor.execute(create_vacancies_table_query)
+    cursor.execute(create_resumes_table_query)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+if __name__ == '__main__':
+    create_tables()
